d7f40e14ad14ab77deda5c275e6f637aa4e044b3
LuizAntonio01 <125375038+LuizAntonio01@users.noreply.github.com>
2023-09-12 11:31:13 -0300

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,13 @@ const router = express.Router();
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
-
+router.get("/cool", function (req, res, next) {
+  res.send("you are so cool");
+});
+router.get("/cool/beach", function (req, res, next) {
+  res.send("beach is so cool");
+});
+router.get("/ifc", function (req, res, next) {
+  res.send("bem-vindo ao IFC");
+});
 module.exports = router;
